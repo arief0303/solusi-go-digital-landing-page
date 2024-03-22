13536d1cb0ced22c4e91cd6cca9b8bf301020c9f
Arief R Syauqie <arief0303@gmail.com>
2024-03-22 15:20:50 +0700

    carousel feature items addition

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -13,6 +13,16 @@ const Carousel = () => {
         description: 'LMS',
         details: 'Learning Management System.',
       },
+      {
+        url: '/images/tagihan-digital.webp',
+        description: 'Tagihan Digital',
+        details: 'Digital billing for your convenience.',
+      },
+      {
+        url: '/images/kartu.webp',
+        description: 'Card Transsaction',
+        details: 'Make payments seamlessly.',
+      },
       /* {
         url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
       },
@@ -72,14 +82,14 @@ const Carousel = () => {
     }, [currentIndex, shouldContinue]);
   
     return (
-      <div className='h-3/4 w-auto m-auto py-0 px-4 relative group pt-8'>
+      <div className='h-[40%] sm:h-3/4 w-auto m-auto py-0 px-4 relative group pt-8 bg-grey'>
         <div
           style={{
             backgroundImage: `url(${slides[currentIndex].url})`, backgroundSize: 'contain',
             backgroundPosition: 'center',
             backgroundRepeat: 'no-repeat'
           }}
-          className='w-full h-1/2 rounded-2xl bg-center bg-cover duration-500'
+          className='w-full h-3/4 rounded-2xl bg-center bg-cover duration-500'
         >
         </div>
         <div className='w-auto h-20 mx-10 pt-7'>
@@ -92,12 +102,12 @@ const Carousel = () => {
         </div>
         {/* Left Arrow */}
         {/* <div className='hidden group-hover:block absolute top-[36%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'> */}
-        <div className='absolute top-[36%] -translate-x-0 translate-y-[-200%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+        <div className='absolute top-1/2 -translate-x-0 translate-y-[-200%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
           <BsChevronCompactLeft onClick={prevSlideBtnClick} size={30} />
         </div>
         {/* Right Arrow */}
         {/* <div className='hidden group-hover:block absolute top-[36%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'> */}
-        <div className='absolute top-[36%] -translate-x-0 translate-y-[-200%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+        <div className='absolute top-1/2 -translate-x-0 translate-y-[-200%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
           <BsChevronCompactRight onClick={nextSlideBtnClick} size={30} />
         </div>
         {/* <div className='flex justify-center py-2 pb-96'>
