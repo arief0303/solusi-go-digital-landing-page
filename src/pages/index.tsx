9269adf6d56733da6290422e0bd9bc7515bafeca
Arief R Syauqie <arief0303@gmail.com>
2024-03-29 14:29:51 +0700

    home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,41 @@
 import React, { useState, useEffect, useRef } from "react";
 import Header from "../components/Header";
+import { Footer } from "../components/Footer";
+import Hero from "../components/Hero";
+import Benefits from "../components/Benefits";
+import Pricing from "../components/Pricing";
+import Partners from "../components/Partners";
+import MobileApp from "../components/MobileApp";
+import { useSpring, animated } from 'react-spring';
 import Carousel from "../components/Carousel";
+import Link from "next/link";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const index = () => {
   return (
     <>
       <Header />
-      <section className="First">
-        <div className="flex flex-col pt-6 sm:pt-10 sm:flex-row bg-gradient-to-b from-base-100 via-base-100 to-grey h-3/4 items-center sm:items-end ">
-          <div className="SolusiGoDigital px-14 py-0 sm:py-20 md:py-40">
-            <img className="h-8" src="/svg/tki.svg" alt="TKI logo" />
-            <div className="my-4 w-full h-auto text-white font-bold leading-normal text-6xl">
-              Solusi GO Digital.
-            </div>
-            <div className="w-18 text-white font-semibold leading-normal text-xl">
-              Ayoo! Bergabung bersama ratusan ribu pelanggan kita yang telah
-              menggunakan produk kami.
-            </div>
-          </div>
-          <img
-            className="max-w-full sm:max-w-[50vw] rounded-t-lg mx-10 mt-5 h-fit"
-            src="/images/preview-desktop.jpg"
-            alt="Preview Desktop App"></img>
-        </div>
+      <section id="First">
+        <Hero />
       </section>
-      <section className="Second">
-        <div className="h-[150vh] sm:h-screen bg-grey">
-          <div className="flex items-center justify-center h-auto w-full font-bold text-6xl text-black px-10 pt-80 pb-0">
-            Solusi inovatif lebih mudah.
-          </div>
-          <Carousel />
-        </div>
+      <section id="Second">
+        <Carousel />
       </section>
+      <section id="Third">
+        <Benefits />
+      </section>
+      <section id="Fourth">
+        <Partners />
+      </section>
+      <section id="Fifth">
+        <Pricing />
+      </section>
+      <section id="Sixth">
+        <MobileApp />
+        <div className="w-screen h-12 bg-grey"></div>
+      </section>
+      <Footer />
     </>
   );
 };
